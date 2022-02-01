71bdc08d138702b6b55b8515e525d2366e4b014f
максат кыдыров <ciber-401@MacBook-Air-maksat.local>
2022-02-01 15:58:58 +0600

    v

--- djangogirls/djangogirls/settings.py
+++ djangogirls/djangogirls/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, '/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'mysite/static')]
 
 
 
@@ -127,4 +127,9 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, '/static')]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'mysite/static')]
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
