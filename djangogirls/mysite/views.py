6ca84d54768bb21bdc7a4786e91c0caf2fcdf590
максат кыдыров <ciber-401@MacBook-Air-maksat.local>
2022-02-01 16:40:58 +0600

    Изменен HTML для сайта.

--- djangogirls/mysite/views.py
+++ djangogirls/mysite/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def post_list(request):
+    return render(request, 'mysite/post_list.html', {})
+
+
+# def post_list(request):
+#     return render(request, 'mysite/post_list.html')
+#
+#
+#
+#
+# def post_list(request):
+#     return render(request, 'mysite/post_list.html')
+#
+#
+#
+#
+# def post_list(request):
+#     return render(request, 'mysite/post_list.html')
+
+
+
+
